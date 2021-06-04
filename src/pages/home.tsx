fa1ab0b06567db4a3b70252375d8c6d43c377ddb
Luis <luis16isasi@gmail.com>
2021-06-04 11:29:48 -0500

    chore: add onSuccess and onError in login

---
+++ src/pages/home.tsx
@@ -0,0 +1,5 @@
+const home = () => {
+  return <div>DESDE HOOME</div>;
+};
+
+export default home;
--- src/views/login/Login.tsx
+++ src/views/login/Login.tsx
@@ -1,9 +1,19 @@
+import { useRouter } from 'next/router';
+
 import UserForm from '@Components/UserForm';
 import Auth from '@Services/auth';
 import Link from '@Components/Links/Link';
 
 const Login = () => {
-  let nextPage = '';
+  const router = useRouter();
+
+  const onSuccess = () => {
+    router.push('/home');
+  };
+
+  const onError = () => {
+    console.error('error');
+  };
 
   return (
     <div className="w-screen h-screen flex justify-center items-center">
@@ -15,15 +25,18 @@ const Login = () => {
           Que bueno verte otra vez :)
         </p>
         <div className="min-w-0 w-full max-w-xs">
-          <UserForm mutation={Auth.loginUser} typeForm="login" />
+          <UserForm
+            mutation={Auth.loginUser}
+            typeForm="login"
+            onSuccess={onSuccess}
+            onError={onError}
+          />
         </div>
         <div className="mt-5">
           <span className="text-white text-xs">
             ¿Aún no tienes una cuenta?
             <Link
-              href={`/register${
-                nextPage !== '/home' ? `?nextPage=${nextPage}` : ''
-              }`}
+              href={'/register'}
               className="text-green-500 font-semibold ml-2"
               text="Regístrate"
             />
